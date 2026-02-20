8fc8470e0c5e5fc23bfa61c2f4601b857e50003f
flaviamedici <flaviaomedici@gmail.com>
2026-02-19 20:23:26 -0800

    Updating AssertionTrue file

--- src/test/java/AssertionTrue.java
+++ src/test/java/AssertionTrue.java
@@ -2,8 +2,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
 
 public class AssertionTrue {
 
@@ -20,8 +22,22 @@ public class AssertionTrue {
     public void testFillEmailFields() {
         WebElement emailField = driver.findElement(By.xpath("//input[@id='input_comp-lkl9bexf2']"));
         emailField.sendKeys("test@email.com");
-        WebElement confirmEmailField = driver.findElement(By.xpath("//input[@id='input_comp-lkl9bexf3']"));
+        
+        WebElement confirmEmailField = driver.findElement(By.xpath("//input[@id='input_comp-lkl9zef6']"));
         confirmEmailField.sendKeys("test@email.com");
         
+        // Get values of both fields
+        String emailValue = emailField.getAttribute("value");
+        String confirmEmailValue = confirmEmailField.getAttribute("value");
+
+        // Assertwhether the values are the same
+        Assert.assertTrue(emailValue.equals(confirmEmailValue), "Email and Confirm Email fields do not match!");
     }   
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
